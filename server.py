9f367ccc310b1a7a16ad5d586b9ba42f2211a890
Jaeung Hyun <jaeung@me.com>
2018-11-05 02:02:35 +0900

    change while loop

--- server.py
+++ server.py
@@ -5,15 +5,16 @@ from socket import *
 
 
 print(temp.byteTemp)
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(('', 12222))
-s.listen(1)
+
 while 1:
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('', 12222))
+    s.listen(1)
+
     print('Connect waiting......')
 
     conn, addr = s.accept()
     print('Connected by', addr)
-
 
     while 1:
         data = conn.recv(1024)

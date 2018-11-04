1cc8e343a4dfbe94c3cc8a5bd9b56e5e5e518006
Jaeung Hyun <jaeung@me.com>
2018-11-05 01:44:37 +0900

    fix bug

--- temp.py
+++ temp.py
@@ -11,5 +11,5 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 if humidity is not None and temperature is not None:
     print("Temp=%0.1f Humidity=%0.1f" % (temperature, humidity))
 
-stringTemp = str(temperature)
-byteTemp = stringTemp.encode() +'\n'
+stringTemp = str(temperature)+'\n'
+byteTemp = stringTemp.encode()

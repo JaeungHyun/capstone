c38bc622fd13913aaa06ff48bcf88f16090a0540
Jaeung Hyun <jaeung@me.com>
2018-11-05 02:22:27 +0900

    use multithread

--- server.py
+++ server.py
@@ -2,27 +2,29 @@
 
 import temp
 from socket import *
+import threading
+
+def handler(clientsock, addr):
+    while 1:
+        data = clientsock.recv(BUFSIZ)
+        if not data : break
+        clientsock.send(temp.byteTemp)
+
+    clientsock.close()
 
 
-print(temp.byteTemp)
+if __name__ == "__main__":
+    HOST = 'localhost'
+    PORT = 12222
+    BUFSIZ = 1024
+    ADDR = (HOST, PORT)
+    serversock = socket(AF_INET, SOCK_STREAM)
+    serversock.bind(ADDR)
+    serversock.listen(2)
 
-while 1:
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind(('', 12222))
-    s.listen(1)
+    while 1:
+        print("Waiting for connection...")
+        clientsock, addr = serversock.accept()
+        print("connected from", addr)
+        t = threading.Thread(handler(clientsock, addr))
 
-    print('Connect waiting......')
-
-    conn, addr = s.accept()
-    print('Connected by', addr)
-
-
-    data = conn.recv(1024)
-    if not data:
-        break
-
-    print('received data :', data)
-    conn.send(temp.byteTemp)
-
-conn.close()
-s.close()

81dea4297eaf6c3e5b60e96c6afa7221529bddd8
Jaeung Hyun <jaeung@me.com>
2018-11-07 23:31:13 +0900

    change open time of solenoid valve

--- Relay.py
+++ Relay.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 pin = 17
 
 def relaycontrol():
     GPIO.setup(pin, GPIO.OUT)  # GPIO Assign mode
     GPIO.output(pin, GPIO.LOW)  # out
+    sleep(10)
     GPIO.output(pin, GPIO.HIGH)  # on

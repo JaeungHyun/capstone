f46bed784a17fcfcf648cbfb712a1ff336e353c1
Jaeung Hyun <jaeung@me.com>
2018-11-19 16:51:41 +0900

    add type casting

--- waterlevel.py
+++ waterlevel.py
@@ -7,6 +7,6 @@ data = GPIO.input(pin)
 
 
 def waterleveling():
-    waterleveling = data + '\n'
+    waterleveling = str(data) + '\n'
     print("Status of Water is %d" % waterleveling)
     return waterleveling
